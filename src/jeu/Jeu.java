45e402707d5ef62e045d6774104f3ed589644060
taith <tai.thanhtran21@gmail.com>
2020-03-17 15:27:24 +0100

    Ajout de la solution

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,15 +1,16 @@
 package jeu;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Jeu {
-	
+    
     private GUI gui; 
-	private Zone zoneCourante;
-	protected Bombe bombe;
-	protected Joueur joueur;
-	private Zone [] zones;
-	private ArrayList<Zone>  zonePrecedente = new ArrayList<Zone>();
+    private Zone zoneCourante;
+    protected Bombe bombe;
+    protected Joueur joueur;
+    private Zone [] zones;
+    private ArrayList<Zone>  zonePrecedente = new ArrayList<Zone>();
 
     
     public Jeu(Joueur joueur) {
@@ -19,17 +20,21 @@ public class Jeu {
         this.joueur = joueur;
     }
 
-    public void setGUI( GUI g) { gui = g; afficherMessageDeBienvenue(); }
+    public boolean setGUI( GUI g) { 
+        gui = g;
+        afficherMessageDeBienvenue(); 
+        return true;
+        }
     
     private void creerCarte() {
-    	Malfaiteur malfaiteur1 = new Malfaiteur("Xavier",
-    			new Enigme("Quand j’avais 6 ans, ma sœur avait la moitié de mon âge. Aujourd’hui j’ai 50 ans, quel âge a ma sœur ? ", "47"));
+        Malfaiteur malfaiteur1 = new Malfaiteur("Xavier",
+                new Enigme("Quand j’avais 6 ans, ma sœur avait la moitié de mon âge. Aujourd’hui j’ai 50 ans, quel âge a ma sœur ? ", "47"));
         Malfaiteur malfaiteur2 = new Malfaiteur("Abd",new Enigme("Quel est le plus grand chiffre du monde ?","9"));
         Malfaiteur malfaiteur3 = new Malfaiteur("Tai",new Enigme("Verticalement : les deuxièmes lettres de chaque mot se suivent avec 2 rangs d’écart. "
-        		+ " \n Horizontalement : les premières lettres de chaque mot se suivent avec 3 rangs d’écart. "
-        		+ "\n Donner la suite des 3 lettres.","PNI"));
+                + " \n Horizontalement : les premières lettres de chaque mot se suivent avec 3 rangs d’écart. "
+                + "\n Donner la suite des 3 lettres.","pni"));
         
-    	zones = new Zone [5];
+        zones = new Zone [5];
         zones[0] = new Zone("centrale", "centre.jpg");
         zones[1] = new Zone("sud", "sud.jpg" );
         zones[2] = new Zone("nord", "nord.jpg",malfaiteur1 );
@@ -66,131 +71,133 @@ public class Jeu {
     }
 
     private void afficherMessageDeBienvenue() {
-    	gui.afficher("Bienvenue !");
-    	gui.afficher();
+        gui.afficher("Bienvenue !");
+        gui.afficher();
         gui.afficher("Tapez '?' pour obtenir de l'aide.");
         gui.afficher();
         afficherLocalisation();
         gui.afficheImage(zoneCourante.nomImage());
     }
     
-    public void traiterCommande(String commandeLue) {
-    	gui.afficher( "> "+ commandeLue + "\n");
-    	String[] result = commandeLue.split(" ");    	
+    public String traiterCommande(String commandeLue) {
+        gui.afficher( "> "+ commandeLue + "\n");
+        String[] result = commandeLue.split(" ");       
         switch (result[0].toUpperCase()) {
         case "?" : case "AIDE" : 
             afficherAide(); 
-        	break;
+            return "?";
         case "Z": case "NORD" :
-        	allerEn( "NORD"); 	
-        	break;
+            allerEn( "NORD");
+            return "NORD";
        case "S" : case "SUD" :
-        	allerEn( "SUD"); 
-        	break;
+            allerEn( "SUD"); 
+            return "SUD";
         case "D" : case "EST" :
-        	allerEn( "EST"); 
-        	break;
+            allerEn( "EST"); 
+            return "EST";
         case "Q" : case "OUEST" :
-        	allerEn( "OUEST"); 
-        	break;
+            allerEn( "OUEST"); 
+            return "OUEST";
         case "P" : case "QUITTER" :
-        	terminer();
-        	break;
+            terminer();
+            return "QUITTER";
         case "DEMANDER":
-        	raconteEnigme();
-        	break;
+            raconteEnigme();
+            return "DEMANDER";
         case "CREDIT":
-        	credit();
-        	break;
+            credit();
+            return "CREDIT";
         case "REPONSE":
-        	if(result.length == 1) gui.afficher("Syntaxe: REPONSE + SOLUTION");
-        	else reponse(result[1]);
-        	break;
+            if(result.length == 1) gui.afficher("Syntaxe: REPONSE + SOLUTION");
+            else reponse(result[1]);
+            return "REPONSE";
         case "DEFUSE":
-        	if(result.length == 1) gui.afficher("Syntaxe: DEFUSE + SOLUTION");
-        	else defuse(result[1]);
-        	break;
+            if(result.length == 1) gui.afficher("Syntaxe: DEFUSE + SOLUTION");
+            else defuse(result[1]);
+            return "DEFUSE";
         case "GONFLER":
-        	gonfler();
-        	break;
+            gonfler();
+            return "GONFLER";
         case "NOM":
-        	getNomJoueur();
-        	break;
+            getNomJoueur();
+            return "NOM";
         case "R" : case "RETOUR":
-        	retour();
-        	break;
-        case "SOLUTION":
+            retour();
+            return "RETOUR";
+        case"T":
         	solution();
-       	default : 
+        default : 
             gui.afficher("Commande inconnue");
-            break;
+            return "DEFAUT";
         }
     }
 
     private void getNomJoueur() {
-    	gui.afficher(joueur.getNom());
+        gui.afficher(joueur.getNom());
     }
     private void gonfler() {
-    	if(zoneCourante == zones[0]) {
-    		if(joueur.getNbrDePieces() < 3) {
-        		gui.afficher("Vous n'avez pas assez de pièces pour activer la machine");
-        		gui.afficher();
-        		gui.afficher("Il te manque " + (3-(joueur.getNbrDePieces())) + " pieces.");
-        	}else {
-        		joueur.setNbrPieces(0);
-        		gui.afficheImage("win.jpg");
-        		zoneCourante.setImage("win.jpg");
-        	}
-    	}else {
-    		gui.afficher("Vous devez être dans la zone principale pour activer la machine");
-    	}
-    	
+        if(zoneCourante == zones[0]) {
+            if(joueur.getNbrDePieces() < 3) {
+                gui.afficher("Vous n'avez pas assez de pièces pour activer la machine");
+                gui.afficher();
+                gui.afficher("Il te manque " + (3-(joueur.getNbrDePieces())) + " pieces.");
+            }else {
+                joueur.setNbrPieces(0);
+                gui.afficheImage("win.jpg");
+                zoneCourante.setImage("win.jpg");
+            }
+        }else {
+            gui.afficher("Vous devez être dans la zone principale pour activer la machine");
+        }
+        
     }
     private void defuse(String str) {
-    	if(zoneCourante == zones[0]) {
-    		if(Integer.parseInt(str) == bombe.getCode()) {
-    			GUI.win();
-    		}else {
-    			GUI.gameOver();
-    		}
-    	}else {
-    		gui.afficher("Vous devez être dans la zone principale pour désamorcer la bombe");
-    	}
+        if(zoneCourante == zones[0]) {
+            if(Integer.parseInt(str) == bombe.getCode()) {
+                Database dbb = new Database();
+                dbb.saveToBDD(joueur.getNom(), bombe.scoreBombe());
+                GUI.win();
+            }else {
+                GUI.gameOver();
+            }
+        }else {
+            gui.afficher("Vous devez être dans la zone principale pour désamorcer la bombe");
+        }
     }
     
     private void reponse(String str) {
-    	if(zoneCourante.getMalfaiteur() != null) {
-    		if(zoneCourante.getMalfaiteur().getEnigme().getReponse().equals(str)) {
-        		if(zoneCourante.getMalfaiteur().getEnigme().getIsDone()) {
-        			gui.afficher("Vous avez déjà répondu à cette énigme.");
-        		}else {
-        			gui.afficher("Bravo tu as gagné une pièce !");
-            		zoneCourante.getMalfaiteur().getEnigme().setDone();
-            		joueur.addPiece();
-        		}
-        	}else {
-        		gui.afficher("MAUVAISE REPONSE , téléportation à la zone principale :( ");
-        		Zone nouvelle = zones[0];
-        		zoneCourante = nouvelle;
-        		gui.afficheImage("centre.jpg");
-        	}
-    	}else gui.afficher("Pas d'enigme ici");
+        if(zoneCourante.getMalfaiteur() != null) {
+            if(zoneCourante.getMalfaiteur().getEnigme().getReponse().equals(str)) {
+                if(zoneCourante.getMalfaiteur().getEnigme().getIsDone()) {
+                    gui.afficher("Vous avez déjà répondu à cette énigme.");
+                }else {
+                    gui.afficher("Bravo tu as gagné une pièce !");
+                    zoneCourante.getMalfaiteur().getEnigme().setDone();
+                    joueur.addPiece();
+                }
+            }else {
+                gui.afficher("MAUVAISE REPONSE , téléportation à la zone principale :( ");
+                Zone nouvelle = zones[0];
+                zoneCourante = nouvelle;
+                gui.afficheImage("centre.jpg");
+            }
+        }else gui.afficher("Pas d'enigme ici");
     }
     
     private void credit() {
-    	gui.afficher("Nombre de pièces : " +joueur.getNbrDePieces());
+        gui.afficher("Nombre de pièces : " +joueur.getNbrDePieces());
     }
     
     private void raconteEnigme() {
-    	if(zoneCourante.getMalfaiteur() != null) {
-    		if(zoneCourante.getMalfaiteur().getEnigme().getIsDone()) {
-    			gui.afficher("Vous avez déjà répondu à cette énigme.");
-    		}else {
-    			gui.afficher(zoneCourante.getMalfaiteur().getEnigme().toString());
-    		}
-    	}else {
-    		gui.afficher("Pas d'enigme dans cette zone !");
-    	}
+        if(zoneCourante.getMalfaiteur() != null) {
+            if(zoneCourante.getMalfaiteur().getEnigme().getIsDone()) {
+                gui.afficher("Vous avez déjà répondu à cette énigme.");
+            }else {
+                gui.afficher(zoneCourante.getMalfaiteur().getEnigme().toString());
+            }
+        }else {
+            gui.afficher("Pas d'enigme dans cette zone !");
+        }
     }
     
     private void afficherAide() {
@@ -203,25 +210,18 @@ public class Jeu {
     }
 
     private void allerEn(String direction) {
-    	Zone nouvelle = zoneCourante.obtientSortie( direction);
-    	if ( nouvelle == null ) {
-        	gui.afficher( "Pas de sortie " + direction);
-    		gui.afficher();
-    	}
-        else {
-        	
-            zonePrecedente.add(zoneCourante);
-        	zoneCourante = nouvelle;
-        	gui.afficher(zoneCourante.descriptionLongue());
-        	gui.afficher();
-        	gui.afficheImage(zoneCourante.nomImage());
-        	raconteEnigme();
+        Zone nouvelle = zoneCourante.obtientSortie( direction);
+        if ( nouvelle == null ) {
+            gui.afficher( "Pas de sortie " + direction);
+            gui.afficher();
         }
-    }
-    
-    private void terminer() {
-    	gui.afficher( "Au revoir...");
-    	gui.enable( false);
+        else {
+            zonePrecedente.add(zoneCourante);
+            zoneCourante = nouvelle;
+            gui.afficher(zoneCourante.descriptionLongue());
+            gui.afficher();
+            gui.afficheImage(zoneCourante.nomImage());
+        }
     }
     
     private void retour() {
@@ -236,37 +236,24 @@ public class Jeu {
     	}
     	
     }
+    private void terminer() {
+        gui.afficher( "Au revoir...");
+        gui.enable( false);
+    }
     
     private void solution() {
-    	//Debut de la solution
-    	gui.afficher( "Debut de la solution");
-    	
-    	//Resolution premiere Egnime au nord
-    	gui.afficher("Direction nord");
-    	allerEn( "NORD"); 	
-    	raconteEnigme();
-
-    	reponse("47");
-    	
-    	//Resolution deuxieme egnime a l'ouest
-    	gui.afficher("Direction ouest");
-    	allerEn("SUD");
-    	allerEn("OUEST");
-    	raconteEnigme();
-    	reponse("PNI");
-    	
-    	//Resolution troisieme egnime a l'est
-    	gui.afficher("Direction est");
-    	allerEn("EST");
-    	allerEn("EST");
-    	raconteEnigme();
-    	reponse("9");
-    	
-    	//Retour au menu principal + gonflement de ballon
-    	allerEn("OUEST");
-    	gui.afficher("Gonflement ballon");
-    	gonfler();
-    	gui.afficher("desamorcage");
-    	defuse("196"); 	
+    	try{
+    	InputStream flux=new FileInputStream("Solution.txt"); 
+    	InputStreamReader lecture=new InputStreamReader(flux);
+    	BufferedReader buff=new BufferedReader(lecture);
+    	String ligne;
+    	while ((ligne=buff.readLine())!=null){
+    		traiterCommande(ligne);
+    	}
+    	buff.close(); 
+    	}		
+    	catch (Exception e){
+    	System.out.println(e.toString());
+    	}
     }
 }
